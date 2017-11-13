202382c5c2d99ed9a57b6a48c107104c6a06985c
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-11-13 11:04:55 +0300

    Fix locale error

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module TestGuru
 
     # Custom settings:
     config.time_zone = "Moscow"
+    I18n.available_locales = [:en, :ru]
     I18n.default_locale = :ru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
