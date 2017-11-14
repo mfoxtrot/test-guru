a5b1ad1d38a3852422ab4fb1d850ec5577ac9940
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-11-14 07:54:32 +0300

    Fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create([{email: 'admin@mail.ru'}, {email: 'user@mail.ru'}])
-Category.create({title: 'Ruby'}, {title: 'SQL'}, {title: 'HTML'})
-Question.create(
-  {body: 'Каким оператором начинается описание функции?'},
-  {body: 'Какая команда используется для создания таблицы?'},
-  {body: 'Какой тег используется для описания заголовка первого уровня?'}
-)
-Answer.create(
+
+Category.create([{title: 'Ruby'}, {title: 'SQL'}, {title: 'HTML'}])
+
+Question.create([
+    {body: 'Каким оператором начинается описание функции?'},
+    {body: 'Какая команда используется для создания таблицы?'},
+    {body: 'Какой тег используется для описания заголовка первого уровня?'}
+])
+
+Answer.create([
   {body: 'def', correct: true},
   {body: 'func', correct: false},
   {body: 'proc', correct: false},
@@ -22,9 +25,10 @@ Answer.create(
   {body: 'h1', correct: true},
   {body: 'header1', correct: false},
   {body: 'title', correct: false}
-)
-Test.create(
+])
+
+Test.create([
   {title: 'Синтаксис Ruby', level: 0},
   {title: 'DDL', level: 1},
   {title: 'Теги', level: 0}
-)
+])
