1aca982f1b9b743d81115ca354d1e949a4213224
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-11-15 23:51:00 +0300

    Add model associations and test_title_by_category class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.tests_titles_by_category(category_title)
+    category_id = Category.find_by(title: category_title)
+    Test.joins(:category).where(categories: {id: category_id}).select(:title).order("tests.title desc")
+  end
 end
