d54c08578850ea4e4971f5b46e6592e110fb04eb
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-11-15 23:07:22 +0300

    Add Test to Category association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113083307) do
+ActiveRecord::Schema.define(version: 20171115200601) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171113083307) do
     t.integer "level", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
