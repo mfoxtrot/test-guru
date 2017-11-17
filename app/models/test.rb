27865249c45b3d2ad0d9dfb3415cc80404e91279
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-11-18 00:38:03 +0300

    Fix remarks II

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,6 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
 
   def self.tests_titles_by_category(category_title)
-    #category_id = Category.find_by(title: category_title)
-    Test.joins(:category).where(categories: {id: Category.find_by(title: category_title)}).order("tests.title desc").pluck(:title)
+    Test.joins(:category).where(categories: {title: category_title}).order("tests.title desc").pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
 
   def tests_by_level(level)
-    self.tests.where(level: level).to_a
+    self.tests.where(level: level)
   end
 end
