4c3612723637ea86ada6b517d7af105c139d3f66
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-11-18 21:58:11 +0300

    Add severail associtations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_and_belongs_to_many :users
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
+
+  has_many :tests_user
+  has_many :users, through: :tests_users
+  has_many :questions
 
   def self.tests_titles_by_category(category_title)
     Test.joins(:category).where(categories: {title: category_title}).order("tests.title desc").pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :tests
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+  has_many :authored_tests, class_name: "Test", foreign_key: "user_id"
 
   def tests_by_level(level)
     self.tests.where(level: level)
