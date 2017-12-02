c8b3fc951eaeeb3bebc79e29d188c789129d813c
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-02 22:58:02 +0300

    Fix notes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,20 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index]
+  before_action :find_test, only: [:index, :create]
   before_action :find_question, only: [:show, :destroy]
-rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    @questions = @test.questions
-    html_string = '<h1>Перечень вопросов:</h1>'
-    @questions.each { |q| html_string << "<li>#{q.body}</li>" }
-    html_string << '<a href="/tests/2/questions/new">Добавить</a>'
-    render html: html_string.html_safe
+    render html: generate_index_page.html_safe
   end
 
   def show
-    html_string = '<h1>Вопрос №' + params[:id].to_str + '</h1>'
-    html_string << '<p>' + @question.body
-    render html: html_string.html_safe
+    render html: generate_show_page.html_safe
   end
 
   def new
   end
 
   def create
-    @question = Question.create(question_params)
+    @test.questions.create(question_params)
   end
 
   def destroy
@@ -42,7 +35,16 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     params.permit(:body, :test_id)
   end
 
-  def record_not_found
-    render plain: '404 Not found', status: 404
+  def generate_index_page
+    html_string = '<h1>Перечень вопросов:</h1>'
+    @test.questions.pluck(:body).each { |b| html_string << "<li>#{b}</li>" }
+    html_string << '<a href="/tests/2/questions/new">Добавить</a>'
+    html_string
+  end
+
+  def generate_show_page
+    html_string = "<h1>Вопрос №#{params[:id]}</h1>"
+    html_string << "<p>#{@question.body}</p>"
+    html_string
   end
 end
