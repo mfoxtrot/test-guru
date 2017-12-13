55088231b2579257e44f6090af227e78c4bedb98
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-14 00:38:31 +0300

    Add question_header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def question_header(persisted_question, test_id)
+    action_title = persisted_question ? "Edit" : "Create new"
+    action_title << " #{Test.find(test_id).title} Question"
+  end
 end
