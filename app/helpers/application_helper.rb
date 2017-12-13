2f9ebd1a3bc5f8dfd7b72cfbc670b36a5241b4aa
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-14 00:44:48 +0300

    Add application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    DateTime.now.year.to_s
+  end
+
+  def github_url(user_name, repo_name)
+    "https://github.com/#{user_name}/#{repo_name}"
+  end
 end
