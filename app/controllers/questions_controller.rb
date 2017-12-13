3e53e904944aee6ce41f2ac31b3f29b4b2d17c00
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-13 22:04:55 +0300

    Add basic CRUD views for question model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,34 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: [:index, :create]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_question, only: [:show, :destroy, :edit, :update]
 
   def index
   end
 
   def show
-    render html: generate_show_page.html_safe
+  end
+
+  def edit
   end
 
   def new
+    @question = Question.new
+  end
+
+  def update
+    @question.update(question_params)
+    redirect_to test_questions_path(@test)
   end
 
   def create
     @test.questions.create(question_params)
+    redirect_to test_questions_path(@test)
   end
 
   def destroy
-    @question.delete
+    @test = @question.test
+    @question.destroy
+    redirect_to test_questions_path(@test)
   end
 
   private
@@ -28,22 +39,11 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+    @test = @question.test
   end
 
   def question_params
-    params.permit(:body, :test_id)
+    params.require(:question).permit(:body, :test_id)
   end
 
-  def generate_index_page
-    html_string = '<h1>Перечень вопросов:</h1><ul>'
-    @test.questions.pluck(:body).each { |b| html_string << "<li>#{b}</li>" }
-    html_string << '</ul><a href="/tests/2/questions/new">Добавить</a>'
-    html_string
-  end
-
-  def generate_show_page
-    html_string = "<h1>Вопрос №#{params[:id]}</h1>"
-    html_string << "<p>#{@question.body}</p>"
-    html_string
-  end
 end
