7e7384317a60ae615a258d43851ec3fc5740acc6
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-16 22:31:58 +0300

    Last fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   end
 
   def github_link(user_name, repo_name)
-    tag.a repo_name, { href: "https://github.com/#{user_name}/#{repo_name}", target: '_blank' }
+    tag.a repo_name, href: "https://github.com/#{user_name}/#{repo_name}", target: '_blank'
   end
 end
