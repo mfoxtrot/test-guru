595b8820325379e1cf1c1bbea57d233f008cc488
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-20 23:00:36 +0300

    Answer controller&views

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true do
+      resources :answers, shallow: true
+    end
   end
 end
