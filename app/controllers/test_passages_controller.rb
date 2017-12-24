e01b46991109d4e7afa05cf87d67a190a51f57e5
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-25 00:23:33 +0300

    Create logit for passing&finish tests

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,4 +1,5 @@
 class TestPassagesController < ApplicationController
+  before_action :set_test_passage, only: [:show, :result, :update]
 
   def show
   end
@@ -7,6 +8,17 @@ class TestPassagesController < ApplicationController
   end
 
   def update
+    @test_passage.accept!(params[:answers_ids])
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
   end
-  
+
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params[:id])
+  end
 end
