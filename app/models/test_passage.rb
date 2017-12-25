ccd35f1688f13b8dafc1b13e9a5c52dbe3ddfc5f
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-12-25 22:23:56 +0300

    Next question in TestPassage via callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,12 +4,12 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def accept!(answers_ids)
     if correct_answer?(answers_ids)
       self.correct_questions += 1
     end
-    self.current_question = next_question
     save!
   end
 
@@ -21,6 +21,10 @@ class TestPassage < ApplicationRecord
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answers_ids)
