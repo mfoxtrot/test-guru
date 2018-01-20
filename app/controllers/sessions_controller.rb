4786555fe6794da09c3d7ca64ab452a7e2c22be5
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-01-20 14:15:03 +0300

    Add session#destroy method & logout path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,9 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
+  get :logout, to: 'sessions#destroy'
 
   resources :users, only: :create
   resources :sessions, only: :create
