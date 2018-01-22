b6af0ee4fd0bf6a98b8dd51840fbc02a2b91e7ab
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-01-23 02:02:06 +0300

    Add after sign in redirection for admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  def after_sign_in_path_for(resource)
+    if current_user.is_a?(Admin)
+      admin_tests_path
+    else
+      super
+    end
+  end
+
   private
 
   def record_not_found
