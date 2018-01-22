a60b53abe3117aa5852949b511b768083b736ee3
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-01-23 00:52:49 +0300

    Modify views for admin namespace

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,12 +1,41 @@
 class Admin::TestsController < ApplicationController
   before_action :authenticate_user!, only: [:show, :start]
-  before_action :find_test, only: [:show, :start]
+  before_action :find_test, only: [:show, :start, :edit, :destroy, :update]
 
   def index
     @tests = Test.all
   end
 
   def show
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    
   end
 
   def start
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to test_path(@question.test)
+      redirect_to admin_test_path(@question.test)
     else
       render :edit
     end
@@ -24,7 +24,7 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to test_path(@test)
+      redirect_to admin_test_path(@test)
     else
       render :new
     end
@@ -32,7 +32,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to test_path(@question.test)
+    redirect_to admin_test_path(@question.test)
   end
 
   private
