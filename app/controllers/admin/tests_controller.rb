1f902b19f49712c59298b2ebc4a6c2f1cfe1ef7a
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-01-24 00:26:47 +0300

    Remove .start method from admin/tests_controller

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -39,11 +39,6 @@ class Admin::TestsController < Admin::BaseController
     redirect_to admin_tests_path
   end
 
-  def start
-    current_user.tests.push(@test)
-    redirect_to current_user.test_passage(@test)
-  end
-
   private
 
   def find_test
