82beb8628cce75c6618335a5741f36c9d9f14b7d
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-01-23 03:27:47 +0300

    Add flash on registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    flash[:notice] = "Привет, #{params[:user][:first_name]}"
+  end
 
   # GET /resource/edit
   # def edit
