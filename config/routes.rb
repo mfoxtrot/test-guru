3c5ff13969e450c3518be4bcc28ee1d4bb9d2883
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-02-06 01:50:40 +0300

    Add gist_controller & routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    resources :gists, only: [:index] 
+
     resources :tests do
       resources :questions, shallow: true, except: :index do
         resources :answers, shallow: true, except: :index
