2f1435ef7efdbf0b7514e2d1e1f027c80ddbe87d
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-02-18 01:11:13 +0300

    Set enable_dependency_loading option to true

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module TestGuru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.enable_dependency_loading = true
     config.autoload_paths << "#{Rails.root}/lib/services"
   end
 end
