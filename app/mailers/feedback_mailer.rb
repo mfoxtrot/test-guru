3245b22257c522656496c513125bb6fee8c87796
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-02-18 04:11:04 +0300

    fix sender address rejected error

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,8 +1,8 @@
 class FeedbackMailer < ApplicationMailer
 
   def message_email(user, message)
-    @from = user.email
+    #@from = user.email
     @message = message
-    Admin.all.each { |admin| mail(from: @from, to: admin.email , subject: "Message from #{@from}") }
+    Admin.all.each { |admin| mail(to: admin.email , subject: "Message from #{@from}") }
   end
 end
