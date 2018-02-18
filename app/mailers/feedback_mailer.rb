9a1e2acc86b418b4e39df2485dc563044d5bf56d
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-02-18 04:01:06 +0300

    Fix errors

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -3,6 +3,6 @@ class FeedbackMailer < ApplicationMailer
   def message_email(user, message)
     @from = user.email
     @message = message
-    Admin.all.each { |admin| mail(from: @from, to: admin.email , subject: "Message from #{from}") }
+    Admin.all.each { |admin| mail(from: @from, to: admin.email , subject: "Message from #{@from}") }
   end
 end
