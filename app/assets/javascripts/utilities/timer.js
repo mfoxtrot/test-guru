2579764e63c822a2a3f7017860f807603e3f3431
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-02-23 12:14:59 +0300

    Format timer output

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -10,11 +10,22 @@ document.addEventListener('turbolinks:load', function(){
 function startTimer(control, timeRemaining) {
   if (timeRemaining > 0) {
     var timerLabel = document.querySelector('.timer_label');
-    timerLabel.textContent = timeRemaining;
+    timerLabel.textContent = formatTime(timeRemaining);
     timeRemaining--;
     setTimeout(startTimer, 1000, control, timeRemaining);
   } else {
     var resultPage = window.location.href + '/result';
     window.location.replace(resultPage);
   }
+}
+
+function formatTime(numberOfSecs) {
+  var secondsToOutput = numberOfSecs % 60
+  var minutesToOutput = (numberOfSecs - secondsToOutput) / 60
+
+  if (secondsToOutput < 10) {
+    secondsToOutput = '0' + secondsToOutput
+  }
+
+  return minutesToOutput + ':' + secondsToOutput
 }
